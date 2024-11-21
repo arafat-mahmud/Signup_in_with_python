c6b59c93fc77aa83fbef05d5f534dc034b4cfbe8
Arafat Mahmud <arafat.mahmud.2001@gmail.com>
2024-11-21 23:19:36 +0600

    check now

--- project/project/settings.py
+++ project/project/settings.py
@@ -7,10 +7,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-c+6=vww6qvqq8=bvhvvl^&r^@!7z93n$zavdtuwcdcwe&w^dhf"
 
 
-DEBUG = True
+DEBUG = False
 
 # ALLOWED_HOSTS = ["*"]
-ALLOWED_HOSTS = ['arafat.up.railway.app', 'localhost', '127.0.0.1:8000']
+ALLOWED_HOSTS = ['arafat.up.railway.app', '127.0.0.1']
 
 # Application definition
 
@@ -107,5 +107,5 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CSRF_TRUSTED_ORIGINS = [
     'https://arafat.up.railway.app',
-    'http://arafat.up.railway.app',  # Add this for non-HTTPS access
+    
 ]
