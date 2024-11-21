088141f2787b53f6a71cefd4b3bf9f1f18db5d44
Arafat Mahmud <arafat.mahmud.2001@gmail.com>
2024-11-21 22:36:56 +0600

    check it now

--- project/project/urls.py
+++ project/project/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from django.urls import path
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('myapp.urls')),
 ]
+
+# Only add this if you're running locally or testing locally
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
